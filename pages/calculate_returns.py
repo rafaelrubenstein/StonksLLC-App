96a3cd6358396c3e1e2ebc771c10453bb0776d8c
rafael rubenstein <73216681+rafaelrubenstein@users.noreply.github.com>
2022-11-23 03:09:01 -0500

    Update calculate_returns.py

--- pages/calculate_returns.py
+++ pages/calculate_returns.py
@@ -5,6 +5,37 @@ import plotly.express as px
 
 with open("/styles.css") as f:
     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+    
+# navbar
+st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
+
+st.markdown("""
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #16A2CB;">
+  <a class="navbar-brand" href="https://stonksllc.streamlit.app" target="_blank">Stonks LLC</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link disabled" href="/">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/calculate_returns">Predict Company's Value</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/visualize_data">Visualize Stock Data</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/about_us"> About Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contact">Contact us</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+""", unsafe_allow_html=True)
 
 
 
