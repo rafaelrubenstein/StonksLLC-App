92fb2665b7ccc7d1f3565760155453c097bbe853
rafael rubenstein <73216681+rafaelrubenstein@users.noreply.github.com>
2022-11-23 17:10:08 -0500

    Update home.py

--- home.py
+++ home.py
@@ -13,26 +13,35 @@ with open("styles.css") as f:
     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
 
 # navbar
-components.html("""
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-<nav class="navbar navbar-expand-lg bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="home">Stonks LLC</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="home">Home</a>
-        <a class="nav-link" href="/calulate_returns">Predit Company's Value</a>
-        <a class="nav-link" href="">About us</a>
-        <a class="nav-link href="">Contact Us</a>
-      </div>
-    </div>
+st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
+
+st.markdown("""
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #16A2CB;">
+  <a class="navbar-brand" href="https://stonksllc.streamlit.app" target="_blank">Stonks LLC</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link disabled" href="/">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/calculate_returns">Predict Company's Value</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/visualize_data">Visualize Stock Data</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/about_us"> About Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contact">Contact us</a>
+      </li>
+    </ul>
   </div>
 </nav>
-""")
+""", unsafe_allow_html=True)
 
 # Create a page header
 st.header("Welcome To The Future Of Investing")
