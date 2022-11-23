71a508f11e1e825583937831b12f0e1ba403f3dc
rafael rubenstein <73216681+rafaelrubenstein@users.noreply.github.com>
2022-11-23 17:13:50 -0500

    Update calculate_returns.py

--- pages/calculate_returns.py
+++ pages/calculate_returns.py
@@ -19,9 +19,6 @@ st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootst
 st.markdown("""
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #16A2CB;">
   <a class="navbar-brand" href="https://stonksllc.streamlit.app" target="_blank">Stonks LLC</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
