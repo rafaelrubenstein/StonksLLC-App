1e0c8ade0df7e7eff03f255d18f652a09927e1e5
yaakoveitan <78498603+yaakoveitan@users.noreply.github.com>
2022-11-23 18:32:07 -0500

    Cleaned interface, just aesthetic changes

--- pages/calculate_returns.py
+++ pages/calculate_returns.py
@@ -46,28 +46,28 @@ st.markdown("""
 knclass = pickle.load(open("models/kneighborclass.pkl", 'rb'))
 
 
-st.write("These numbers can be found on the company's balance sheet, if a company has 400,000,000 assets please enter as 400")
+st.write("Enter the following items found on a companies balance sheet. Units are in millions.")
 
-act = st.number_input("Enter the companys Total Current Assets",key=1)
+act = st.number_input("Total Current Assets",key=1)
 
-at = st.number_input("Enter the companys Current Assets",key=2)
+at = st.number_input("Current Assets",key=2)
 
-che = st.number_input("Enter the companys Cash and short term investments",key=3)
+che = st.number_input("Cash and short term investments",key=3)
 
-dltt = st.number_input("Enter the companys Total Longterm Debt",key=4)
+dltt = st.number_input("Total Longterm Debt",key=4)
 
-intan = st.number_input("Enter the companys Total Intangible assets",key=5)
+intan = st.number_input("Total Intangible assets",key=5)
 
-lct = st.number_input("Enter the companys Total Current Liabilities",key=6)
+lct = st.number_input("Total Current Liabilities",key=6)
 
-lt = st.number_input("Enter the companys Total Liabilities",key=7)
+lt = st.number_input("Total Liabilities",key=7)
 
-rect = st.number_input("Enter the companys Total Recievables",key=8)
+rect = st.number_input("Total Recievables",key=8)
 
-st.write("This can be found on the income statement")
-spi = st.number_input("Enter the companys Special items",key=9)
+st.write("Income Statement Items")
+spi = st.number_input("Special items",key=9)
 
-mkvalt = st.number_input("Enter the companys Current Market value",key=10)
+mkvalt = st.number_input("Current Market value",key=10)
 
 make_prediction = st.button("Predict if stock is undervalued")
 
@@ -77,6 +77,6 @@ if make_prediction:
     prediction = knclass.predict([to_predict])
 
     if prediction:
-        st.write("The stock is undervalued! You should invest in it")
+        st.write("The stock is likely undervalued! You should invest in it")
     else:
         st.write("This stock is not undervalued")
