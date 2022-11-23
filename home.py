dc1a3d1727a7fc9547b553205dbb5efb3d321bea
rafael rubenstein <73216681+rafaelrubenstein@users.noreply.github.com>
2022-11-23 16:53:46 -0500

    Update home.py

--- home.py
+++ home.py
@@ -13,7 +13,7 @@ with open("styles.css") as f:
 
 # navbar
 st.markdown('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">', unsafe_allow_html=True)
-st.markdown('<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>', unsafe_allow_html=True)
+
 
 st.markdown("""
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #16A2CB;">
@@ -55,3 +55,4 @@ with col1:
 with col2:
 
     st.image("StockData.png")
+st.markdown('<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>', unsafe_allow_html=True)
