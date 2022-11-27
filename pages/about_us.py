ee57bf07315c9c68e7240c419a7ad7680186ea68
rafael rubenstein <73216681+rafaelrubenstein@users.noreply.github.com>
2022-11-27 13:35:54 -0500

    added other pages and css
    testing css for coming soon pages

---
+++ pages/about_us.py
@@ -0,0 +1,41 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="About us",
+    page_icon="💸",
+    initial_sidebar_state="collapsed",
+    layout = "centered",
+)
+
+with open("styles.css") as f:
+    st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+    
+# navbar
+st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
+
+st.markdown("""
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #16A2CB;">
+  <a class="navbar-brand" href="https://stonksllc.streamlit.app" target="_blank">Stonks LLC</a>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link disabled" href="/">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/calculate_returns">Predict Company's Value</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/visualize_data">Visualize Stock Data</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/about_us"> About Us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contact">Contact us</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+""", unsafe_allow_html=True)
+
+
