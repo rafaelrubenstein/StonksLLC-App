d54e2dfa1e872aa9537f6fb9fa60480ee5934360
rafael rubenstein <73216681+rafaelrubenstein@users.noreply.github.com>
2022-12-06 16:51:46 -0500

    Update calculate_returns.py

--- pages/calculate_returns.py
+++ pages/calculate_returns.py
@@ -22,26 +22,26 @@ st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootst
 
 st.markdown("""
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #16A2CB;">
-  <a class="navbar-brand" href="https://stonksllc.streamlit.app" target="_blank">Stonks LLC</a>
+  <a class="navbar-brand" href="https://stonksllc.streamlit.app" target= "_self">Stonks LLC</a>
    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link disabled" href="/">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link disabled" href="/" target= "_self">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="/calculate_returns">Predict Company's Value</a>
+        <a class="nav-link" href="/calculate_returns" target= "_self" >Predict Company's Value</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="/visualize_data">Visualize Stock Data</a>
+        <a class="nav-link" href="/visualize_data" target= "_self">Visualize Stock Data</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="/about_us"> About Us</a>
+        <a class="nav-link" href="/about_us" target= "_self"> About Us</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="contact">Contact us</a>
+        <a class="nav-link" href="contact" target= "_self">Contact us</a>
       </li>
     </ul>
   </div>
@@ -128,17 +128,6 @@ ticker = st.text_input("Ticker for company an example is the apple ticker","aapl
 market_value, msft = scrape_os_and_calulate_mkvalt(ticker)
 
 financial_data = clean_balance_sheet_for_feautures(msft,market_value)
-
-
-# act = financial_data.get('Total Current Assets'0)
-# at = financial_data.get('Total Assets',0)
-# che = financial_data.get('Cash',0)
-# dltt = financial_data.get('Long Term Debt',0)
-# intan = financial_data.get('Intangible Assets',0)
-# lct = financial_data.get('Total Current Liabilities',0)
-# lt = financial_data.get('Total Liab',0)
-# rect = financial_data.get('Net Receivables',0)
-# mkvalt = financial_data.get('Market Value',0)
 
 
 make_prediction = st.button("Predict if stock is undervalued")
