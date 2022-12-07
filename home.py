3cd25940a66c2c676a4186663a3985edbbedf7b8
rafael rubenstein <73216681+rafaelrubenstein@users.noreply.github.com>
2022-12-07 01:28:03 -0500

    Update home.py

--- home.py
+++ home.py
@@ -17,7 +17,7 @@ with open("styles.css") as f:
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 
 st.markdown("""
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #16A2CB;">
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #04AA6D;">
   <a class="navbar-brand" href="home" target= "_self">Stonks LLC</a>
    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
